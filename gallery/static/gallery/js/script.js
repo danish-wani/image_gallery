060aad5f9182e0651703a5bb2806f6e8ec85e682
danish-wani <danish.wani@trialx.com>
2021-03-14 15:49:42 +0530

    -Delete confirmation added

--- gallery/static/gallery/js/script.js
+++ gallery/static/gallery/js/script.js
@@ -12,3 +12,14 @@ var category = document.getElementById("category-dropdown").value ;
 Array.from(elements).forEach(function(element) {
   element.setAttribute("href", element.getAttribute("href") + "&category="+category);
 });
+
+
+$('.fa-trash').click(function(){
+
+    let image_pk = $(this).attr('data-image-pk');
+
+    let url = '/images/' + image_pk + '/';
+
+    $('#deleteImage-form').attr('action', url);
+
+})
